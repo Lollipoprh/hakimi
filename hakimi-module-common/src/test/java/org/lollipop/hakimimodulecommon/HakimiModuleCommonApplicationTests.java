764f985cf526b22135ad790e90eb4ee0dd9d8db0
lollipopr <17726362182@163.com>
2026-03-12 22:06:24 +0800

    初始化项目

---
+++ hakimi-module-common/src/test/java/org/lollipop/hakimimodulecommon/HakimiModuleCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lollipop.hakimimodulecommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakimiModuleCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ hakimi-service-conversation/src/main/java/org/lollipop/ConversationApplication.java
@@ -0,0 +1,11 @@
+package org.lollipop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConversationApplication.class, args);
+    }
+}
